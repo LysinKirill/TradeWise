b8af0ce399dba12feec06e8754be2f713a448d06
klysin <klysin@ozon.ru>
2025-05-23 11:48:47 +0300

    [master]: fix: use python3 and install python dependencies in train script

--- train_models.sh
+++ train_models.sh
@@ -5,5 +5,6 @@ cd TradeWise-python
 
 NUM_MODELS=${1:-5}  # Default to 5 if no argument provided
 
+pip3 install -r requirements.txt
 
-python -m ml.training.run_training --config-dir "./ml/training/settings/oneMinute" --num-models $NUM_MODELS
+python3 -m ml.training.run_training --config-dir "./ml/training/settings/oneMinute" --num-models $NUM_MODELS
